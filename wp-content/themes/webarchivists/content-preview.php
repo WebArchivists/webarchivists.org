6fbdd23b7d5b155616fa32450d0aee8cb718bd87
Denis Hovart <dhovart@gmail.com>
2012-05-09 01:59:52 +0200

    Continuation de l’intégration wordpress

--- wp-content/themes/webarchivists/content-preview.php
+++ wp-content/themes/webarchivists/content-preview.php
@@ -8,20 +8,26 @@
  * @subpackage Webarchivists
  */
 ?><article id="post-<?php the_ID(); ?>" <?php post_class( 'indexed' ); ?>>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'webarchivists' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-    <div class="entry-content">
-    	<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'webarchivists' ) ); ?>
-    </div>
-    <div class="entry-meta">
-    	<?php
-    		printf( __( '<a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s" rel="author">%6$s</a></span></span>', 'webarchivists' ),
-    			esc_url( get_permalink() ),
-    			get_the_date( 'c' ),
-    			get_the_date(),
-    			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-    			esc_attr( sprintf( __( 'View all posts by %s', 'webarchivists' ), get_the_author() ) ),
-    			get_the_author()
-    		);
-    	?>
-    </div>
+    <header>
+        <!-- <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'webarchivists' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2> -->
+        <div class="meta">
+            <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark" class="date">
+                <time pubdate="<?php echo get_the_date( 'c' ) ?>" datetime=""><?php
+                    $dateformat = __( 'F j<\s\u\p>S</\s\u\p>', 'webarchivists' );
+        		    echo date_i18n( $dateformat, strtotime( get_the_date( 'c' ) ) );
+                ?></time>
+            </a>
+            <?php webarchivists_comments_info() ?>
+            <span class="country de">Germany</span>
+        </div>
+        <h2>
+            <a href="<?php echo esc_url( get_permalink() ) ?>">
+                <?php the_title() ?>
+            </a>
+        </h2>
+    </header>
+    <p>
+        <?php echo get_the_excerpt() ?>
+    	<a href="<?php echo esc_url( get_permalink() ) ?>#join-the-discussion" class="more"><?php echo __( 'discuss', 'webarchivists' ) ?></a>
+    </p>
 </article>
