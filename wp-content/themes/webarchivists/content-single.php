f82e6aaa886b29562cf8c5429e80bf7f2cde0221
Denis Hovart <dhovart@gmail.com>
2012-09-09 22:34:56 +0200

    Modifications page article

--- wp-content/themes/webarchivists/content-single.php
+++ wp-content/themes/webarchivists/content-single.php
@@ -25,13 +25,15 @@
         <?php the_post_thumbnail() ?>
         <dl>
             <dt>Published:</dt>
-            <dd><time datetime="<?php echo get_the_date( 'c' ) ?>" pubdate><?php the_date() ?></time> <time datetime="<?php echo get_the_modified_date( 'c' ) ?>" class="lastrev">(last revision: <?php echo the_modified_date() ?>)</time></dd>
+            <dd><time datetime="<?php echo get_the_date( 'c' ) ?>" pubdate><?php the_date() ?></time><?php if (get_the_date('c') !== get_the_modified_date('c')): ?> <time datetime="<?php echo get_the_modified_date( 'c' ) ?>" class="lastrev">(last revision: <?php echo the_modified_date() ?>)</time><?php endif ?></dd>
 
             <dt>Author(s):</dt>
             <dd><?php the_author() ?></dd>
 
+            <?php /*
             <dt>Organization:</dt>
             <dd>Webarchivists (FR)</dd>
+            */ ?>
             
             <?php if(get_the_tags()): ?>
             <dt>Tags:</dt>
@@ -46,55 +48,6 @@
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'webarchivists' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div>
-
-
-
-    <?php /*
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma * /
-			$categories_list = get_the_category_list( __( ', ', 'webarchivists' ) );
-
-			/* translators: used between list items, there is a space after the comma * /
-			$tag_list = get_the_tag_list( '', __( ', ', 'webarchivists' ) );
-			if ( '' != $tag_list ) {
-				$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'webarchivists' );
-			} elseif ( '' != $categories_list ) {
-				$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'webarchivists' );
-			} else {
-				$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'webarchivists' );
-			}
-
-			printf(
-				$utility_text,
-				$categories_list,
-				$tag_list,
-				esc_url( get_permalink() ),
-				the_title_attribute( 'echo=0' ),
-				get_the_author(),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-			);
-		?>
-		<?php edit_post_link( __( 'Edit', 'webarchivists' ), '<span class="edit-link">', '</span>' ); ?>
-
-		<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
-		<div id="author-info">
-			<div id="author-avatar">
-				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'webarchivists_author_bio_avatar_size', 68 ) ); ?>
-			</div><!-- #author-avatar -->
-			<div id="author-description">
-				<h2><?php printf( __( 'About %s', 'webarchivists' ), get_the_author() ); ?></h2>
-				<?php the_author_meta( 'description' ); ?>
-				<div id="author-link">
-					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'webarchivists' ), get_the_author() ); ?>
-					</a>
-				</div><!-- #author-link	-->
-			</div><!-- #author-description -->
-		</div><!-- #entry-author-info -->
-		<?php endif; ?>
-	</footer><!-- .entry-meta -->
-	*/ ?>
 
     <?php comments_template( '', true ); ?>
 
