98bb54daff5ae037914c4de9b6549dd7c7135a23
Denis Hovart <dhovart@gmail.com>
2012-10-22 18:30:12 +0200

    analytics

--- wp-content/themes/webarchivists/footer.php
+++ wp-content/themes/webarchivists/footer.php
@@ -59,5 +59,15 @@
 <?php wp_footer(); ?>
 <script src="<?php bloginfo('template_directory') ?>/js/libs/jquery-1.7.1.min.js"></script>
 <script src="<?php bloginfo('template_directory') ?>/js/scripts.js"></script>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-393906-18");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
 </body>
 </html>
