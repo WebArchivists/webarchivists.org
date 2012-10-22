a678a82971b5d34d75a1868707e4584d6406ac76
Hans Lemuet <hans@spone.net>
2012-10-22 18:13:42 +0300

    Added thumbnail to content-page

--- wp-content/themes/webarchivists/content-page.php
+++ wp-content/themes/webarchivists/content-page.php
@@ -8,6 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php the_post_thumbnail() ?>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
