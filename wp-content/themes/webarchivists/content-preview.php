9ecbd4473f1a183b5e62f7ec156eb4121c56c63d
Denis Hovart <dhovart@gmail.com>
2012-10-22 17:20:50 +0200

    affichage post de type liens sur la home, rectifications

--- wp-content/themes/webarchivists/content-preview.php
+++ wp-content/themes/webarchivists/content-preview.php
@@ -9,16 +9,16 @@
  */
 ?><article id="post-<?php the_ID(); ?>" <?php post_class( 'indexed' ); ?>>
     <header>
-        <?php if (has_post_thumbnail( get_the_id() ) ):
-        $thumbnail_info = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_id() ), 'category-thumb', false );    
-        ?>
-        <a href="<?php echo esc_url( get_permalink() ) ?>" class="thumbnail" style="background-image:url(<?php echo  $thumbnail_info[0]; ?>)"></a>
-        <?php endif;  ?>
-        <div class="meta">
         <?php
             if( get_post_type() == 'link' ) $permalink = esc_url( get_post_meta($post->ID, "link_url", true) );
             else $permalink = esc_url( get_permalink() );
         ?>
+        <?php if (has_post_thumbnail( get_the_id() ) ):
+        $thumbnail_info = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_id() ), 'category-thumb', false );    
+        ?>
+        <a href="<?php echo $permalink ?>" class="thumbnail" style="background-image:url(<?php echo $thumbnail_info[0]; ?>)"></a>
+        <?php endif;  ?>
+        <div class="meta">
             <a href="<?php echo $permalink  ?>" rel="bookmark" class="date">
                 <time pubdate="<?php echo get_the_date( 'c' ) ?>" datetime=""><?php
                     $dateformat = __( 'F j<\s\u\p>S</\s\u\p>', 'webarchivists' );
