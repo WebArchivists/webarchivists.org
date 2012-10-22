d6cd2ce32d7968d571162d706788024cb147d314
Denis Hovart <dhovart@gmail.com>
2012-10-22 17:07:11 +0200

    modifications pour affichage des liens sur home

--- wp-content/themes/webarchivists/content-preview.php
+++ wp-content/themes/webarchivists/content-preview.php
@@ -15,25 +15,33 @@
         <a href="<?php echo esc_url( get_permalink() ) ?>" class="thumbnail" style="background-image:url(<?php echo  $thumbnail_info[0]; ?>)"></a>
         <?php endif;  ?>
         <div class="meta">
-            <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark" class="date">
+        <?php
+            if( get_post_type() == 'link' ) $permalink = esc_url( get_post_meta($post->ID, "link_url", true) );
+            else $permalink = esc_url( get_permalink() );
+        ?>
+            <a href="<?php echo $permalink  ?>" rel="bookmark" class="date">
                 <time pubdate="<?php echo get_the_date( 'c' ) ?>" datetime=""><?php
                     $dateformat = __( 'F j<\s\u\p>S</\s\u\p>', 'webarchivists' );
         		    echo date_i18n( $dateformat, strtotime( get_the_date( 'c' ) ) ) . (get_the_date('Y') != date('Y') ? ' '.get_the_date('Y') : '');
                 ?></time>
             </a>
-            <?php webarchivists_comments_info() ?>
+            <?php if( get_post_type() !== 'link' ) webarchivists_comments_info() ?>
             <?php /* 
             <span class="country de">Germany</span>
             */ ?>
         </div>
         <h2>
-            <a href="<?php echo esc_url( get_permalink() ) ?>">
+            <a href="<?php echo $permalink ?>">
                 <?php the_title() ?>
             </a>
         </h2>
     </header>
+    <?php if( get_post_type() === 'link' ) : ?>
+        <p><?php echo get_the_content() ?></p>
+    <?php else : ?>
     <p>
         <?php echo get_the_excerpt() ?>
     	<a href="<?php echo esc_url( get_permalink() ) ?>#join-the-discussion" class="more"><?php echo __( 'discuss', 'webarchivists' ) ?></a>
     </p>
+    <?php endif; ?>
 </article>
